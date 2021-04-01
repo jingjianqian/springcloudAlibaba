e574e07c29a4af33bc4a9ad4028a843dde4a2af6
jingjianqian <18697998680@163.com>
2021-04-01 22:06:05 +0800

    eureka简单集群

--- consumer-order80/src/main/java/top/jingjianqian/springcloud/OrderSpringApp.java
+++ consumer-order80/src/main/java/top/jingjianqian/springcloud/OrderSpringApp.java
@@ -3,10 +3,12 @@ package top.jingjianqian.springcloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Slf4j
+@EnableDiscoveryClient
 public class OrderSpringApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderSpringApp.class,args);
--- provider-payment8001/src/main/java/top/jingjianqian/springcloud/PaymentMain8001.java
+++ provider-payment8001/src/main/java/top/jingjianqian/springcloud/PaymentMain8001.java
@@ -2,9 +2,11 @@ package top.jingjianqian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
