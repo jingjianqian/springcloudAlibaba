116197d53a4acd7ea69add4bd228b34819190c2a
jingjianqian <18697998680@163.com>
2021-04-07 22:13:11 +0800

    简单集群 补充业务代码

---
+++ provider-payment8002/src/main/java/top/jingjianqian/springcloud/providerPayment8002/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package top.jingjianqian.springcloud.providerPayment8002.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import top.jingjianqian.common.springcloud.entities.Payment;
+
+@Mapper
+public interface PaymentDao {
+
+    public  int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long  id);
+}
