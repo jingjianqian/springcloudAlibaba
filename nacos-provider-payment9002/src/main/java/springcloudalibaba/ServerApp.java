d9155dd049dafc7ebffae99e04780bed6568d133
jingjianqian <18697998680@163.com>
2021-07-21 23:38:04 +0800

    服务发现之nacos-集群

--- nacos-provider-payment9002/src/main/java/springcloudalibaba/ServerApp.java
+++ nacos-provider-payment9002/src/main/java/springcloudalibaba/ServerApp.java
@@ -1,4 +1,4 @@
-package top.jingjianqian.springcloudalibaba;
+package springcloudalibaba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
