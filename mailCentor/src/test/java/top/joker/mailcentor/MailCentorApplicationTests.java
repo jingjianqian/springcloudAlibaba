96185684a9bac7c0fcb51ba1c2437fa29a68db7f
jingjianqian <18697998680@163.com>
2022-11-28 19:38:07 +0800

    add send mail demo

--- mailCentor/src/test/java/top/joker/mailcentor/MailCentorApplicationTests.java
+++ mailCentor/src/test/java/top/joker/mailcentor/MailCentorApplicationTests.java
@@ -1,11 +1,15 @@
 package top.joker.mailcentor;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootTest
 class MailCentorApplicationTests {
 
+    @Autowired
+    private JavaMailSenderImpl mailSender;
     @Test
     void contextLoads() {
     }
