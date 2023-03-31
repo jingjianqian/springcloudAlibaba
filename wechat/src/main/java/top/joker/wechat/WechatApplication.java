9f14c6159775bd684dc95d29d2bc1553102f3650
joker <18697998680@163.com>
2023-03-31 09:37:36 +0800

    nacos服务注册

--- wechat/src/main/java/top/joker/wechat/WechatApplication.java
+++ wechat/src/main/java/top/joker/wechat/WechatApplication.java
@@ -1,14 +1,9 @@
 package top.joker.wechat;
 
-import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
-import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableNacosDiscovery
-@NacosPropertySource(dataId = "example", autoRefreshed = true,type = ConfigType.TEXT)
 public class WechatApplication {
 
     public static void main(String[] args) {
